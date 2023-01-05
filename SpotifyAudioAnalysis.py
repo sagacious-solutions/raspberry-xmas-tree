25b6b500851c6aab1d6220de0af9f1af537df114
Matt Macdonald <mksmacdonald@gmail.com>
2023-01-05 09:54:56 -0800

    implemented binary search to reduce time spent parsing audio data

--- SpotifyAudioAnalysis.py
+++ SpotifyAudioAnalysis.py
@@ -1,4 +1,5 @@
 import time
+from typing import Dict
 from config import log
 
 ONE_SECOND_MILLISECONDS = 1000
@@ -14,7 +15,7 @@ class SpotifyAudioAnalysis:
         tatums,
         track,
         track_progress,
-        lag_time_ms,
+        lag_time_ms=0,
         **kwargs,
     ):
         self.bars = bars
@@ -25,8 +26,6 @@ class SpotifyAudioAnalysis:
         self.track_duration = track["duration"]
         self.track_progress_ms = track_progress + lag_time_ms
         self.created_time = time.time()
-
-        log.debug(f"The lag time was {lag_time_ms}")
 
     def get_track_progress_seconds(self) -> float:
         """Gets the current track progress in seconds
@@ -39,23 +38,100 @@ class SpotifyAudioAnalysis:
 
         return new_track_time
 
-    def active_thingies(self):
-        track_time_seconds = self.get_track_progress_seconds()
+    def get_active_linear_search(
+        self, progress_seconds: int = None
+    ) -> Dict[str, dict]:
+        """Gets the current active items from spotify data using a linear search
+
+        Args:
+            progress_seconds (int, optional): What point in track to check whats active.
+                Only exists for consistency in testing. Defaults to None.
+
+        Returns:
+            dict: The current active items for the given progress in the song
+        """
+        track_time_seconds = (
+            self.get_track_progress_seconds()
+            if not progress_seconds
+            else progress_seconds
+        )
 
         active = {
-            "bar": self.__get_active(self.bars, track_time_seconds),
-            "beat": self.__get_active(self.beats, track_time_seconds),
-            "section": self.__get_active(self.sections, track_time_seconds),
-            "segment": self.__get_active(self.segments, track_time_seconds),
-            "tatum": self.__get_active(self.tatums, track_time_seconds),
+            "bar": self.__linear_search_active(self.bars, track_time_seconds),
+            "beat": self.__linear_search_active(self.beats, track_time_seconds),
+            "section": self.__linear_search_active(
+                self.sections, track_time_seconds
+            ),
+            "segment": self.__linear_search_active(
+                self.segments, track_time_seconds
+            ),
+            "tatum": self.__linear_search_active(
+                self.tatums, track_time_seconds
+            ),
         }
 
         return active
 
-    def __get_active(self, category, track_time_seconds):
-        for item in category:
+    def __linear_search_active(self, category, track_time_seconds):
+        for i, item in enumerate(category):
             start = item["start"]
             stop = start + item["duration"]
 
             if start <= track_time_seconds <= stop:
-                return item
+                return {"index": i, **item}
+
+    def get_active_binary_search(
+        self, progress_seconds: int = None
+    ) -> Dict[str, dict]:
+        """Gets the current active items from spotify data using a binary search
+
+        Args:
+            progress_seconds (int, optional): What point in track to check whats active.
+                Only exists for consistency in testing. Defaults to None.
+
+        Returns:
+            dict: The current active items for the given progress in the song
+        """
+        track_time_seconds = (
+            self.get_track_progress_seconds()
+            if not progress_seconds
+            else progress_seconds
+        )
+
+        active = {
+            "bar": self._binary_search_active(self.bars, track_time_seconds),
+            "beat": self._binary_search_active(self.beats, track_time_seconds),
+            "section": self._binary_search_active(
+                self.sections, track_time_seconds
+            ),
+            "segment": self._binary_search_active(
+                self.segments, track_time_seconds
+            ),
+            "tatum": self._binary_search_active(
+                self.tatums, track_time_seconds
+            ),
+        }
+
+        return active
+
+    def _binary_search_active(self, data_arr, track_time_seconds):
+        low = 0
+        high = len(data_arr) - 1
+        mid = 0
+
+        while low <= high:
+            mid = (high + low) // 2
+            current = data_arr[mid]
+            start = current["start"]
+            end = start + current["duration"]
+
+            if end < track_time_seconds:
+                low = mid + 1
+
+            elif start > track_time_seconds:
+                high = mid - 1
+
+            else:
+                return {"index": mid, **data_arr[mid]}
+
+        return None
