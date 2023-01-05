b34d8feb0248ac71ffdba2844e68d4eccdee3b16
Matt Macdonald <mksmacdonald@gmail.com>
2023-01-05 09:55:35 -0800

    added extra song for test and parameratized tests

--- tests/test_audio_analysis.py
+++ tests/test_audio_analysis.py
@@ -1,26 +1,45 @@
 import pickle
 import pytest
 from pathlib import Path
+import time
 
 from SpotifyAudioAnalysis import SpotifyAudioAnalysis
 
 
-@pytest.fixture
-def StairwayToHeavenSpotifyData():
-    filepath = Path.cwd() / "tests" / "stairwayToHeavenAnalysis.dict"
-
-    with open(filepath, "rb") as file:
-        return pickle.load(file)
+track_data_files = [
+    "stairwayToHeavenAnalysis.dict",
+    "frontierPsychiatristAnalysis.dict",
+]
 
 
-@pytest.fixture
-def SongAnalysisClass(StairwayToHeavenSpotifyData):
+@pytest.fixture(params=track_data_files)
+def SongAnalysisClass(request):
     TRACK_PROGRESS_SECS = 120
 
+    filepath = Path.cwd() / "tests" / request.param
+
+    with open(filepath, "rb") as file:
+        track_data = pickle.load(file)
+
     return SpotifyAudioAnalysis(
-        track_progress=TRACK_PROGRESS_SECS, **StairwayToHeavenSpotifyData
+        track_progress=TRACK_PROGRESS_SECS, **track_data
     )
 
 
-def test_time_to_get_active(SongAnalysisClass):
-    print(SongAnalysisClass)
+@pytest.mark.parametrize("time_seconds", [45, 120, 146, 199, 330])
+def test_time_to_get_active(SongAnalysisClass, time_seconds):
+    linear_start = time.time()
+    linear_search_active = SongAnalysisClass.get_active_linear_search(
+        progress_seconds=time_seconds
+    )
+    linear_search_time = time.time() - linear_start
+
+    binary_start = time.time()
+    binary_search_active = SongAnalysisClass.get_active_binary_search(
+        progress_seconds=time_seconds
+    )
+    binary_search_time = time.time() - binary_start
+
+    assert linear_search_active == binary_search_active
+    # Make sure binary search is at least twice as fast as linear
+    assert binary_search_time < linear_search_time / 2
